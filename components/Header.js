bb2f0d85f133fbb07cc962f573956fb4c35748c1
leanonchingwe <66792863+leanonchingwe@users.noreply.github.com>
2023-09-14 16:42:31 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,37 @@ export default function Header({ name }) {
           <a>{name}</a>
         </Link>
       </p>
+
+      {/* Navigation Links */}
+      <nav className="text-center mt-4">
+        <ul className="flex justify-center space-x-6">
+          <li>
+            <Link href="/">
+              <a className="text-blue-500 hover:text-blue-700">Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/categories">
+              <a className="text-blue-500 hover:text-blue-700">Categories</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className="text-blue-500 hover:text-blue-700">About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a className="text-blue-500 hover:text-blue-700">Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Optional Search Bar */}
+      <div className="mt-4 text-center">
+        {/* Add your search bar component here if needed */}
+      </div>
     </header>
   );
 }
